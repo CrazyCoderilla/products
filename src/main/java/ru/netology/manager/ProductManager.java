d9cf33b836b8b65d243896dcdf2a66bd33ec8106
test <test>
2021-08-28 18:14:16 +0300

    d2 fix

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -29,20 +29,4 @@ public class ProductManager {
         return result;
     }
 
-    public boolean matches(Product product, String search) {
-        if (product instanceof Book) { // если в параметре product лежит объект класса Book
-            Book book = (Book) product; // положем его в переменную типа Book чтобы пользоваться методами класса Book
-            if (book.getAuthor().contains(search) || book.getName().contains(search)) {
-                return true;
-            }
-        }
-        if (product instanceof Smartphone) {
-            Smartphone smartphone = (Smartphone) product;
-            if (smartphone.getManufacturer().contains(search) || smartphone.getName().contains(search)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
